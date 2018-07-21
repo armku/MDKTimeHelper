d8275f75f58ce7fb4807a4a48cc0b341c9892176
Ace <258613763@qq.com>
2018-07-21 16:14:13 +0800

    增加部分代码

---
+++ Src/MDKTimeHelper/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MDKTimeHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello {0}\n",DateTime.Now);
+        }
+    }
+}
